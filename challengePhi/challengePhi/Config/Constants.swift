8b361d5b69a55d301b00c1ee1b2cb145747c29d0
Angela Alves <angelavbalves@rede.ulbra.br>
2022-08-26 13:53:24 -0300

    feat: Constants colors

---
+++ challengePhi/challengePhi/Config/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  challengePhi
+//
+//  Created by Angela Alves on 23/08/22.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct Color {
+        static let gray = UIColor(red: 0.51, green: 0.51, blue: 0.51, alpha: 1.00)
+        static let darkGray = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.00)
+        static let lightGray = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.00)
+        static let cyan = UIColor(red: 0.00, green: 0.76, blue: 0.69, alpha: 1.00)
+    }
+}
